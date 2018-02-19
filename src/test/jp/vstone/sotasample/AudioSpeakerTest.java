98eaa9b73fb2107ded486328863e5a8f680705a1
WeihangHuang <weihang.huang.16@ucl.ac.uk>
2018-02-19 16:26:32 +0000

    finished version 1, add one test

---
+++ src/test/jp/vstone/sotasample/AudioSpeakerTest.java
@@ -0,0 +1,26 @@
+package jp.vstone.sotasample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by apple on 2/19/18.
+ */
+public class AudioSpeakerTest {
+    @Test
+    public void canSetCorrectFilePath(){
+        AudioSpeaker speaker = new AudioSpeaker();
+        speaker.tellFirstStoryPiece("story_one");
+        assertEquals(speaker.getNextFilePath(),"./audio_resources/story_one/1.wav");
+    }
+
+    @Test
+    public void counterCanIncrement(){
+        AudioSpeaker speaker = new AudioSpeaker("story_one", 2);
+        assertEquals(speaker.getNextFilePath(), "./audio_resources/story_one/3.wav");
+        speaker.tellNextStoryPiece();
+        speaker.tellNextStoryPiece();
+        assertEquals(speaker.getNextFilePath(), "./audio_resources/story_one/5.wav");
+    }
+}
